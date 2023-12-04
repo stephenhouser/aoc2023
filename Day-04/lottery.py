75c27d8780c05208f8d85c72b82d2116319899ab
Stephen Houser <stephenhouser@gmail.com>
2023-12-04 07:30:27 -0500

    day 4 part 2, initial solution

--- Day-04/lottery.py
+++ Day-04/lottery.py
@@ -8,9 +8,8 @@ import re
 from itertools import chain
 from functools import reduce
 
-
 def parse_card(card_line):
-    """Returns a card (card, (winning numbers), (your numbers))
+    """Returns a card (card, (winning numbers), (your numbers), multiplier)
 
     """
     #Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
@@ -18,7 +17,7 @@ def parse_card(card_line):
     card_id = int(card_match.group(1))
     winning_ns = list(map(int, re.findall(r'\d+', card_match.group(2))))
     card_ns = list(map(int, re.findall(r'\d+', card_match.group(3))))
-    return (card_id, winning_ns, card_ns)
+    return [card_id, winning_ns, card_ns, 1]
 
 def load_cards(filename: str) -> list[str]:
     """Load ...
@@ -45,7 +44,27 @@ def score_cards_1(cards):
     """Return a list with all the scored cards"""
     return list(map(score_card_1, cards))
 
-#cards = load_cards('test-1.txt')
+def score_card_2(card):
+    return len(set(card[1]) & set(card[2]))
+               
+def score_cards_2(cards):
+    n_cards = 0
+    for card_n, card in enumerate(cards):
+        (_, win_ns, card_ns, copies) = card
+        n_cards += copies
+
+        match_count = len(set(win_ns) & set(card_ns))
+        for _ in range(copies):
+            for copy_n in range(card_n, card_n + match_count + 1):
+                if copy_n < len(cards):
+                    cards[copy_n][3] += 1
+
+    return n_cards
+
 cards = load_cards('input.txt')
-card_values = score_cards_1(cards)
-print(sum(card_values))
+#cards = load_cards('input.txt')
+#card_values = score_cards_1(cards)
+#print(sum(card_values))
+
+card_values = score_cards_2(cards)
+print(card_values)
