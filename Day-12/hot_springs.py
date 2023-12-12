00ec41fd6e7251214d44ff05cf2f410c147e116d
Stephen Houser <stephenhouser@gmail.com>
2023-12-12 13:52:50 -0500

    Day 12, Part 2

--- Day-12/hot_springs.py
+++ Day-12/hot_springs.py
@@ -7,10 +7,10 @@ Stephen Houser <stephenhouser@gmail.com>
 import re
 import argparse
 import unittest
-import logging as log
-log.basicConfig(format='%(levelname)s: %(message)s', level=log.INFO)
-import math
-import itertools
+from itertools import repeat
+from functools import reduce
+
+from performance import perf_timer, memoized
 
 class TestAOC(unittest.TestCase):
     """Test Advent of Code"""
@@ -29,115 +29,82 @@ class TestAOC(unittest.TestCase):
     #
     # Part Two
     #
-    # def test_part2_example(self):
-    #     """Test example 1 data from test.txt"""
-    #     self.assertEqual(count_records('test.txt', 5), 525152)
+    def test_part2_example(self):
+        """Test example 1 data from test.txt"""
+        self.assertEqual(count_records('test.txt', 5), 525152)
 
-    # def test_part2_solution(self):
-    #     """Test example 1 data from test.txt"""
-    #     self.assertEqual(count_records('input.txt', 5), 3920437278260)
+    def test_part2_solution(self):
+        """Test example 1 data from test.txt"""
+        self.assertEqual(count_records('input.txt', 5), 3920437278260)
 
-import collections
-import functools
-
-class memoized(object):
-    '''Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    '''
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        # if not isinstance(args, collections.Hashable):
-        #     # uncacheable. a list, for instance.
-        #     # better to not cache than blow up.
-        #     return self.func(*args)
-        if args in self.cache:
-            return self.cache[args]
-        else:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
-    def __repr__(self):
-        '''Return the function's docstring.'''
-        return self.func.__doc__
-    def __get__(self, obj, objtype):
-        '''Support instance methods.'''
-        return functools.partial(self.__call__, obj)
-
-
-# obs = "..#."
-# seq = (1, 2, 3)
+#
+# Cache results by memoizing the function
+#
 @memoized
-def matches_for(observations, sequence, tabs=0, show=False):
-    if show: print('\t'*tabs, f'({observations}, {sequence})', end='')
+def matches_for(observations, sequence):
+    """Return the number of possible sequence matches for observations
+    """
+    #print(observations, sequence)
 
+    # Base case: ran out of sequences to match
     if len(sequence) == 0:
-        no = observations.replace('.', '').replace('?', '')
-        if len(no) == 0:
-            # only dots and ? left
-            if show: print('\nONLY DOTS and ?')
-            return len(no) == 0
-        return 0
-        
+        remaining = observations.replace('.', '').replace('?', '')
+        return len(remaining) == 0  # Good sequence if observations ran out
+
+    # Base case: ran out of observations to match, but still have sequences
     if len(observations) == 0:
-        if show: print('\nOUT OF SEQUENCES')
         return 0
 
+    # Recursive case: the next chararcter is ., skip it
     if observations[0] == '.':
-        if show: print()
-        ans = matches_for(observations[1:], sequence, tabs)
-        #print(f'{ans}: No observations or seqences left')
-        return ans
+        return matches_for(observations[1:], sequence)
 
-    # this chunk matches current sequence needs
+    # Recursive case: the next character is #, there *must* be a sequence here
     if re.match('#[?#]{' + str(sequence[0]-1) + '}', observations):
-        if show: print(f' Match on #')
-        next = observations[sequence[0]:]
-        if len(next):
-            if next[0] == '#':
-                return 0
-            if next[0] == '?':
-                next = '.' + next[1:]
+        remaining = observations[sequence[0]:]  # keep what's left
 
-        ans = matches_for(next, sequence[1:], tabs+1)
-        if show: print('\t'*tabs, f'={ans}: match on #')
-        return ans
+        # this path fails if '#' is next
+        if len(remaining) and remaining[0] == '#':
+            return 0
 
+        # if next is ? make . to be a valid as we just ended a sequence
+        if len(remaining) and remaining[0] == '?':
+            remaining = '.' + remaining[1:]
+
+        # find out if the rest matches as well...
+        return matches_for(remaining, sequence[1:])
+
+    # Recursive case: the next character wildcard, track down both options
     if observations[0] == '?':
-        if show: print(' Encounter ?, recurse')
-        ans =  matches_for('#'+observations[1:], sequence, tabs+1) + \
-               matches_for('.'+observations[1:], sequence, tabs+1)
-        if show: print('\t'*tabs, f'={ans}: recursion on choice')
-        return ans
-
+        return matches_for('#'+observations[1:], sequence) + \
+               matches_for('.'+observations[1:], sequence)
 
     return 0
 
 def expand_records(records, expansion):
-    """Unfold the records by the given expansion factor
+    """Unfold each record in the list by the given expansion factor.
     """
-    return map(lambda r:
-        ('?'.join(itertools.repeat(r[0], expansion)), r[1] * expansion),
-        records
-    )
-    # nr = []
-    # for (o, s) in records:
-    #     nr.append(('?'.join(itertools.repeat(o, expansion)), s * expansion))
-
-    # return nr
-
-def count_records(filename, expansion=1):
-    records = expand_records(load_file(filename), expansion)
-    ans = 0
+    expanded = []
     for record in records:
-        result = matches_for(record[0], tuple(record[1]))
-        #print(f'{result:5}: {record}')
-        ans += result
+        (obs, seq) = record
+        exp_obs = '?'.join(repeat(obs, expansion))
+        exp_seq = tuple(seq * expansion)
+        expanded.append((exp_obs, exp_seq))
 
-    return ans
+    return expanded
+
+def count_matches(record):
+    """Return the number of possible sequences that are possible
+       from the given record = (observations, sequences)
+    """
+    (observations, sequences) = record
+    return matches_for(observations, sequences)
+
+#@perf_timer
+def count_records(filename, expansion=1):
+    """Return the count of possible"""
+    records = expand_records(load_file(filename), expansion)
+    return reduce(lambda a, c: a + count_matches(c), records, 0)
 
 def load_file(filename: str):
     """Load lines from file into ___
@@ -170,5 +137,5 @@ def main():
         print()
 
 if __name__ == '__main__':
-    main()
-    #unittest.main()
+    #main()
+    unittest.main()
