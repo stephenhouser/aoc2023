3461b703a2793402376d9b6f7a5fb82f9e5a5edb
Stephen Houser <stephenhouser@gmail.com>
2023-12-13 09:17:17 -0500

    Day 13, part 2 -- the ugly version

--- Day-13/mirrors.py
+++ Day-13/mirrors.py
@@ -45,40 +45,92 @@ def test_function(filename, *args):
 
 def reflections_line(row, candidates):
     """Returns possible reflections"""
-    new_c = []
+    reflections = []
     for i in candidates:
         left = list(reversed(row[:i]))
         right = row[i:]
-        #print(left, "   ", right)
-        #print(list(map(lambda x: x[0]==x[1], zip(left, right))))
         if all(map(lambda x: x[0]==x[1], zip(left, right))):
-            new_c.append(i)
+            reflections.append(i)
 
-    #print(new_c)
-    return new_c
+    return reflections
+
+def repair_map(island_map, smudge):
+    (sx, sy) = smudge
+
+
+def find_smudge(island_map):
+    print()
+
+    cm = 0
+    rm = 0
+
+    col_n = len(island_map[0])
+    row_n = len(island_map)
+
+    col_mirrors = range(1, col_n)
+    cols = []
+    #print('COLS')
+    for row in island_map:
+        # narrows down candidates
+        refs = reflections_line(row, col_mirrors)
+        print(row, refs)
+        cols.extend(reflections_line(row, col_mirrors))
+
+    # smudge would be row with len(island_map[0]) occurrences
+    col_bins = list(map(cols.count, range(len(island_map[0]))))
+    # if the smudge were fixed this is where the mirror would be
+    if row_n-1 in col_bins:
+        cm = col_bins.index(row_n-1)
+        print(f'found smudge at col {cm}')
+    else:
+        print(f'no col smudge')
+
+
+    #print('ROWS')
+    row_mirrors = range(1, row_n)
+    rows = []
+    for col in list(map(list, zip(*island_map))):
+        # narrows down candidates
+        refs = reflections_line(col, row_mirrors)
+        print(''.join(col), refs)
+        rows.extend(reflections_line(col, row_mirrors))
+
+    # smudge would be row with len(island_map[0]) occurrences
+    row_bins = list(map(rows.count, range(len(island_map))))
+    # if the smudge were fixed this is where the mirror would be
+    if col_n-1 in row_bins:
+        rm = row_bins.index(col_n-1)
+        print(f'found smudge at row {rm}')
+    else:
+        print(f'no row smudge')
+
+    if cm == 0 and rm == 0:
+        print(f'HERE ({len(col_bins)}, {len(row_bins)}): {row_bins}, {col_bins}')
+        print_grid(island_map)
+        print()
+
+    return (rm * 100, cm)       # 29763  26595
 
 def count_reflections(island_map):
     # all columns are candidates
-    col_mirrors = list(range(len(island_map[0])))
+    col_mirrors = list(range(1, len(island_map[0])))
     for row in island_map:
         # narrows down candidates
         col_mirrors = reflections_line(row, col_mirrors)
 
-    #row_mirrors = list(range(0, len(island_map)-1))
-    row_mirrors = list(range(len(island_map)))
+    row_mirrors = list(range(1, len(island_map)))
     for col in list(map(list, zip(*island_map))):
         row_mirrors = reflections_line(col, row_mirrors)
 
+    print(f'mirrors: {col_mirrors}, {row_mirrors}')
     cm = sum(col_mirrors)
     rm = sum(row_mirrors)
     return (rm * 100, cm)
 
 def print_grid(grid):
     for r in grid:
-        for c in r:
-            print(c, end='')
-        print()
-
+        print(''.join(r))
+        
 def load_file(filename: str):
     """Load lines from file into ___
         
@@ -92,7 +144,8 @@ def load_file(filename: str):
             for block in map_blocks:
                 this_map = []
                 for line in block.split('\n'):
-                    this_map.append(list(line))
+                    #this_map.append(list(line))
+                    this_map.append(line)
 
                 maps.append(this_map)
 
@@ -124,6 +177,10 @@ def main():
         #
         # Part Two
         #
+        reflections = list(map(find_smudge, maps))
+        reflection_sum = [sum(x) for x in zip(*reflections)]
+        ref_summary = reflection_sum[0] + reflection_sum[1]
+        print(f'\tNumber of reflections: {ref_summary}') #29366
 
         print()
 
