01b707c5f2c72f9321fbb6e77fa63d1415f7b883
Stephen Houser <stephenhouser@gmail.com>
2023-12-13 13:02:35 -0500

    Day 13, initial cleanup

--- Day-13/mirrors.py
+++ Day-13/mirrors.py
@@ -10,6 +10,7 @@ import unittest
 import logging as log
 log.basicConfig(format='%(levelname)s: %(message)s', level=log.INFO)
 from functools import reduce
+import itertools
 
 class TestAOC(unittest.TestCase):
     """Test Advent of Code"""
@@ -19,137 +20,97 @@ class TestAOC(unittest.TestCase):
     #
     def test_part1_example(self):
         """Test example 1 data from test.txt"""
-        self.assertEqual(test_function('test.txt', 2), 0)
+        self.assertEqual(test_function('test.txt'), 405)
 
     def test_part1_solution(self):
         """Live data for part 1 data from input.txt"""
-        self.assertEqual(test_function('input.txt', 200), 0)
+        self.assertEqual(test_function('input.txt'), 37718)
 
     #
     # Part Two
     #
     def test_part2_example(self):
         """Test example 1 data from test.txt"""
-        self.assertEqual(test_function('test.txt', 10), 0)
+        self.assertEqual(test_function('test.txt', 1), 400)
 
     def test_part2_solution(self):
         """Test example 1 data from test.txt"""
-        self.assertEqual(test_function('input.txt', 1000000), 0)
+        self.assertEqual(test_function('input.txt', 1), 40995)
 
 
-def test_function(filename, *args):
+def test_function(filename, smudges=0):
     """Loads sample data and calculates answer...
     """
-    stuff = load_file(filename)
-    return len(stuff) * args[0]
+    maps = load_file(filename)
+    return sum((map(lambda x: count_reflections(x, smudges), maps)))
 
-def reflections_line(row, candidates):
-    """Returns possible reflections"""
+def count_reflections(island_map, smudges):
+    (reflection_col, reflection_row) = find_reflections(island_map, smudges)
+    return reflection_col * 100 + reflection_row
+
+def find_reflections_single(line):
+    """Returns all possible reflection points across a single line"""
+    #print(line)
     reflections = []
-    for i in candidates:
-        left = list(reversed(row[:i]))
-        right = row[i:]
+    for i in range(1, len(line)):   # start at offset 1 to end of line
+        left = list(reversed(line[:i]))
+        right = line[i:]
         if all(map(lambda x: x[0]==x[1], zip(left, right))):
             reflections.append(i)
 
     return reflections
 
-def repair_map(island_map, smudge):
-    (sx, sy) = smudge
+def find_reflections(island_map, smudges):
+    """Returns calculated"""
 
 
-def find_smudge(island_map):
-    print()
-
-    cm = 0
-    rm = 0
-
-    col_n = len(island_map[0])
-    row_n = len(island_map)
-
-    col_mirrors = range(1, col_n)
-    cols = []
-    #print('COLS')
-    for row in island_map:
-        # narrows down candidates
-        refs = reflections_line(row, col_mirrors)
-        print(row, refs)
-        cols.extend(reflections_line(row, col_mirrors))
-
-    # smudge would be row with len(island_map[0]) occurrences
-    col_bins = list(map(cols.count, range(len(island_map[0]))))
-    # if the smudge were fixed this is where the mirror would be
-    if row_n-1 in col_bins:
-        cm = col_bins.index(row_n-1)
-        print(f'found smudge at col {cm}')
-    else:
-        print(f'no col smudge')
 
 
-    #print('ROWS')
-    row_mirrors = range(1, row_n)
-    rows = []
-    for col in list(map(list, zip(*island_map))):
-        # narrows down candidates
-        refs = reflections_line(col, row_mirrors)
-        print(''.join(col), refs)
-        rows.extend(reflections_line(col, row_mirrors))
+    #
+    # Vertical Mirrors
+    #
+    # possible vertical reflection points, one for each row
+    reflections = map(find_reflections_single, island_map)
+    # merge them into one flat list
+    reflections = tuple(itertools.chain(*reflections))
+    # make histogram buckets
+    bins = tuple(map(reflections.count, range(len(island_map[0]))))
+    # look for bin with len(row) elements for a mirror;
+    #    this reflection occurs in every row
+    # look for bin with len(row)-1 elements for a fixable mirror;
+    #   this reflection occurs in all but one rows.
+    #   if we fix the one, it will be a reflection
+    look_for =  len(island_map) - smudges
+    reflection_col = bins.index(look_for) if look_for in bins else 0
 
-    # smudge would be row with len(island_map[0]) occurrences
-    row_bins = list(map(rows.count, range(len(island_map))))
-    # if the smudge were fixed this is where the mirror would be
-    if col_n-1 in row_bins:
-        rm = row_bins.index(col_n-1)
-        print(f'found smudge at row {rm}')
-    else:
-        print(f'no row smudge')
+    #
+    # Horizontal Mirrors
+    #
+    transposed_map =  tuple(map(list, zip(*island_map)))
+    # possible horizontal reflection points, one for each row
+    reflections = map(find_reflections_single, transposed_map)
+    # merge them into one flat list
+    reflections = tuple(itertools.chain(*reflections))
+    # make histogram buckets
+    bins = tuple(map(reflections.count, range(len(transposed_map[0]))))
 
-    if cm == 0 and rm == 0:
-        print(f'HERE ({len(col_bins)}, {len(row_bins)}): {row_bins}, {col_bins}')
-        print_grid(island_map)
-        print()
+    look_for = len(transposed_map) - smudges
+    reflection_row = bins.index(look_for) if look_for in bins else 0
 
-    return (rm * 100, cm)       # 29763  26595
-
-def count_reflections(island_map):
-    # all columns are candidates
-    col_mirrors = list(range(1, len(island_map[0])))
-    for row in island_map:
-        # narrows down candidates
-        col_mirrors = reflections_line(row, col_mirrors)
-
-    row_mirrors = list(range(1, len(island_map)))
-    for col in list(map(list, zip(*island_map))):
-        row_mirrors = reflections_line(col, row_mirrors)
-
-    print(f'mirrors: {col_mirrors}, {row_mirrors}')
-    cm = sum(col_mirrors)
-    rm = sum(row_mirrors)
-    return (rm * 100, cm)
+    return (reflection_row, reflection_col)
 
 def print_grid(grid):
+    """Pretty print 2D grid in readable form"""
     for r in grid:
-        print(''.join(r))
-        
+        for c in r:
+            print(c, end='')
+        print()
+
 def load_file(filename: str):
-    """Load lines from file into ___
-        
-       filename: the file to read game descriptions from.
-       returns: ...
-    """
+    """Load lines from file into 2D list"""
     try:
         with open(filename, 'r', encoding='utf-8') as file:
-            map_blocks = file.read().split('\n\n')
-            maps = []
-            for block in map_blocks:
-                this_map = []
-                for line in block.split('\n'):
-                    #this_map.append(list(line))
-                    this_map.append(line)
-
-                maps.append(this_map)
-
-            return maps
+            return tuple(map(lambda x: x.split('\n'), file.read().split('\n\n')))
 
     except FileNotFoundError:
         log.error('File %s not found.', filename)
@@ -185,5 +146,5 @@ def main():
         print()
 
 if __name__ == '__main__':
-    main()
-    #unittest.main()
+    #main()
+    unittest.main()
