e2dfcbbef80b74850754040bb9ce1df3733070e4
Stephen Houser <stephenhouser@gmail.com>
2023-12-14 11:35:28 -0500

    Day 14, more cleanup

--- Day-14/parabolic_dish.py
+++ Day-14/parabolic_dish.py
@@ -9,7 +9,7 @@ import argparse
 import unittest
 import logging as log
 import hashlib
-import sys
+import itertools
 
 class TestAOC(unittest.TestCase):
     """Test Advent of Code"""
@@ -18,49 +18,35 @@ class TestAOC(unittest.TestCase):
     # Part One
     #
     def test_part1_example(self):
-        """Test example 1 data from test.txt"""
-        self.assertEqual(test_function('test.txt', 2), 0)
+        """Test example 1 from test.txt"""
+        weight = calculate_weight(roll_north(load_file('test.txt')))
+        self.assertEqual(weight, 136)
 
     def test_part1_solution(self):
-        """Live data for part 1 data from input.txt"""
-        self.assertEqual(test_function('input.txt', 200), 0)
+        """Solution for part 2 from input.txt"""
+        weight = calculate_weight(roll_north(load_file('input.txt')))
+        self.assertEqual(weight, 109596)
 
     #
     # Part Two
     #
     def test_part2_example(self):
-        """Test example 1 data from test.txt"""
-        self.assertEqual(test_function('test.txt', 10), 0)
+        """Test example 1 from test.txt"""
+        weight = calculate_weight(
+            roll_cycles(load_file('test.txt'), 1_000_000_000))
+        self.assertEqual(weight, 64)
 
     def test_part2_solution(self):
-        """Test example 1 data from test.txt"""
-        self.assertEqual(test_function('input.txt', 1000000), 0)
-
+        """Solution for part 2 from input.txt"""
+        weight = calculate_weight(
+            roll_cycles(load_file('input.txt'), 1_000_000_000))
+        self.assertEqual(weight, 96105)
 
 def test_function(filename, *args):
     """Loads sample data and calculates answer...
     """
     stuff = load_file(filename)
     return len(stuff) * args[0]
-
-
-class Thing:
-    """Represents a ___"""
-    def __init__(self, text):
-        self._parse_line(text)
-
-    def _parse_line(self, text):
-        """Parse text description of ___"""
-        match = re.match(r'\d+', text)
-
-    def __repr__(self):
-        """Return REPL representation"""
-        return str(self)
-
-    def __str__(self):
-        """Return string representation"""
-        return f'Empty'
-
 
 def print_grid(grid):
     """Pretty print 2D grid in readable form"""
@@ -70,10 +56,10 @@ def print_grid(grid):
         print()
 
 def transpose(grid):
-    return list(map(list, zip(*grid)))
+    return tuple(map(tuple, zip(*grid)))
 
 def flip(grid):
-    return list(map(list, map(reversed, grid)))
+    return tuple(map(tuple, map(reversed, grid)))
 
 def roll_row(row):
     #print(row)
@@ -84,7 +70,7 @@ def roll_row(row):
         n_row += 'O' * block.count('O') + \
                  '.' * block.count('.') + \
                  '#' * block.count('#')
-    return list(n_row)
+    return n_row
 
 #   N  W  S    E
 #   tt x tfft  f
@@ -92,7 +78,7 @@ def roll_cycle(dish):
     # north
     dish = transpose(map(roll_row, transpose(dish)))
     # west
-    dish = list(map(roll_row, dish))
+    dish = map(roll_row, dish)
     # south
     dish = transpose(flip(map(roll_row, flip(transpose(dish)))))
     #east
@@ -100,17 +86,47 @@ def roll_cycle(dish):
 
     return dish
 
-def hash_value(dish):
-    char_map = ''
-    for r in dish:
-        char_map += ''.join(r)
-    
-    return hashlib.sha256(char_map.encode()).hexdigest()
+def roll_cycles(dish, cycles):
+    """Simulate rolling `dish` through N,W,S,E cycles and return
+        the final dish arrangement after `cycles`
+    """
+    arrangements = {}               # cache past arrangement (hash values)
+
+    # First, find the cycle in the arrangements
+    cycle = (None, None)            # (start, length) of cycle (result of loop)
+    for i in range(cycles):         # start counting at 1 end before cycles
+        dish_key = map_hash(dish)   # have we seen this arrangement before
+        if dish_key in arrangements:
+            prev = arrangements[dish_key]
+            cycle = (prev, i-prev)
+            break                   # stop the madness
+
+        arrangements[dish_key] = i  # save the arrangement
+        dish = roll_cycle(dish)     # roll it around
+
+    # The target cycle is `end_offset` rolls from the start of the repeating
+    # cycle. We can just play that many more forward and end at the same 
+    # arrangement we would have if we played thorough all the actual cycles
+    # This is remaining cycles % cycle_length
+    end_offset = (cycles - cycle[0]) % cycle[1]
+
+    # Restart rolling until we get to the cycle we are looking for
+    #   ...I could have saved these arrangements, but... cheap to replay
+    for i in range(end_offset):
+        dish = roll_cycle(dish)
+
+    return dish
+
+def map_hash(dish):
+    """Returns an SHA256 hash value for the map arrangement
+        Used to index saved values for hopefully faster lookup
+    """
+    text = ''.join(itertools.chain(*dish))
+    return hashlib.sha256(text.encode()).hexdigest()
 
 def roll_north(dish):
     dish = transpose(map(roll_row, transpose(dish)))
     return dish
-
 
 def calculate_weight(dish):
     weight = 0
@@ -128,9 +144,7 @@ def load_file(filename: str):
     try:
         with open(filename, 'r', encoding='utf-8') as file:
             # read a grid of text into 2D grid
-            return list(map(list,map(str.strip, file.readlines())))
-            # read a list of Thing
-            #return list(map(Thing, file.readlines()))
+            return tuple(map(tuple, map(str.strip, file.readlines())))
 
     except FileNotFoundError:
         log.error('File %s not found.', filename)
@@ -157,38 +171,9 @@ def main():
         #
         # Part Two
         #
-        cycles = 1_000_000_000
-
-        cycle_start = None
-        cycle_length = None
-        arrangements = {}
-        for i in range(1, cycles+1):  # start counting at 1 end before cycles
-            dish_key = hash_value(dish)
-            if dish_key in arrangements:
-                (prev, dish) = arrangements[dish_key]
-                print(f'Cycle {i} repeats cycle {prev}', end=' ')
-                cycle_start = prev
-                cycle_length = i - prev
-                break
-            else:
-                dish = roll_cycle(dish)
-
-            if cycle_start:
-                print(f'weight={calculate_weight(dish)}')
-
-            arrangements[dish_key] = (i, dish)
-
-        print(f'Arrangements repeat starting at {cycle_start} and every {cycle_length} cycles')
-
-        # offset restart to where we can restart the cycle
-        # and it will hit the
-        skip_cycles = (cycles - cycle_start) / cycle_length
-        restart = cycle_start + (int(skip_cycles) * cycle_length)
-        for i in range(restart+1, cycles+1):
-            dish = roll_cycle(dish)
-
+        dish = roll_cycles(dish, 1_000_000_000)
         weight = calculate_weight(dish)
-        print(f'\t2. The weight on the north beam ({i}): {weight}')
+        print(f'\t2. The weight on the north beam: {weight}')
         #print_grid(dish)
         #print()
 
