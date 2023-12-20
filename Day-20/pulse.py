d06e4e07ba32ade25b7a2f7ce15eb4aa3e3b29c6
Stephen Houser <stephenhouser@gmail.com>
2023-12-20 15:42:03 -0500

    Day 20, part 1

--- Day-20/pulse.py
+++ Day-20/pulse.py
@@ -7,6 +7,7 @@ Stephen Houser <stephenhouser@gmail.com>
 import re
 import argparse
 import unittest
+from collections import deque
 from cProfile import Profile
 
 
@@ -35,25 +36,44 @@ class TestAOC(unittest.TestCase):
         """Test example 1 data from test.txt"""
         self.assertEqual(test_function('input.txt', 1000000), 0)
 
-
-def test_function(filename, *args):
-    """Loads sample data and calculates answer...
-    """
-    stuff = load_file(filename)
-    return len(stuff) * args[0]
-
-
 class LogicNode:
     """Represents a ___"""
     def __init__(self, text):
         self.name = None
+        self.gate = None
         self.state = False
-        self.inputs = []
+        self.inputs = {}
         self.outputs = []
         self._parse_line(text)
 
+    node_re = re.compile(r'^([%&]?)([a-z]+)\s+->\s+(.*)$')
+    output_re = re.compile(r'[a-z]+')
     def _parse_line(self, text):
-        pass
+        match = LogicNode.node_re.match(text)
+        self.gate = match.group(1)
+        self.name = match.group(2).strip()
+        self.outputs = LogicNode.output_re.findall(match.group(3))
+
+    def add_input(self, who_from, initial_value=False):
+        self.inputs[who_from] = initial_value
+
+    def input(self, who_from, pulse):
+        match self.gate:
+            case '%':   # flip flop
+                # trigger change on low
+                if not pulse:
+                    self.state = not self.state
+                    return self.state
+            case '&':   # nand
+                # trigger low if all inputs are high
+                self.inputs[who_from] = pulse
+                self.state = not all(self.inputs.values())
+                return self.state
+            case '':  # broadcaster
+                # send low on "press"
+                return pulse
+
+        return None
 
     def __repr__(self):
         """Return REPL representation"""
@@ -61,37 +81,61 @@ class LogicNode:
 
     def __str__(self):
         """Return string representation"""
-        return f'{self.name}({self.inputs})->{self.state}'
-
-def print_grid(grid):
-    """Pretty print 2D grid in readable form"""
-    for row in grid:
-        for col in row:
-            print(col, end='')
-        print()
+        return f'{self.gate}{self.name}:{self.state} in={self.inputs} out={self.outputs}'
 
 def load_file(filename: str):
-    """Load lines from file into ___
-        
-       filename: the file to read game descriptions from.
-       returns: ...
-    """
+    """Load lines from file into ___"""
     try:
         with open(filename, 'r', encoding='utf-8') as file:
-            # read a grid of text into 2D grid
-            return list(map(list,map(str.strip, file.readlines())))
+            circuit = {}
+            for line in file.readlines():
+                node = LogicNode(line)
+                circuit[node.name] = node
 
-            # read a list of Thing
-            #return list(map(Thing, file.readlines()))
+            # wire up inputs based on outputs
+            gates = list(circuit.values())
+            for gate in gates:
+                for output_gate in gate.outputs:
+                    if output_gate in circuit:
+                        circuit[output_gate].add_input(gate.name)
+                    else:
+                        circuit[output_gate] = LogicNode(f'{output_gate} -> ')
 
-            # returns a dictionary indexed by i,j with c as the value
-            # return {complex(i,j): c for j, r in enumerate(file)
-            #                     for i, c in enumerate(r.strip())}
+            return circuit
 
     except FileNotFoundError:
         print('File %s not found.', filename)
 
     return []
+
+def print_circuit(circuit):
+    for gate in circuit:
+        print(circuit[gate])
+
+def press_button(circuit, press_times=1):
+    low_pulses = 0
+    high_pulses = 0
+
+    for _ in range(press_times):
+        pulses = deque([('button', 'broadcaster', False)])
+        while pulses:
+            from_gate, to_gate, pulse = pulses.pop()
+            #print(f'{from_gate} : {pulse} -> {to_gate}')
+            if pulse:
+                high_pulses += 1
+            else:
+                low_pulses += 1
+
+            gate = circuit[to_gate]
+            output_pulse = gate.input(from_gate, pulse)
+
+            if output_pulse is not None:
+                for output_gate in gate.outputs:
+                    pulses.appendleft((to_gate, output_gate, output_pulse))
+
+
+    print(f'low={low_pulses}, high={high_pulses}')
+    return low_pulses * high_pulses
 
 def main():
     """Main Routine, does all the work"""
@@ -104,13 +148,13 @@ def main():
         print(filename)
 
         with Profile() as profile:
-            things = load_file(filename)
+            circuit = load_file(filename)
 
             #
             # Part One
             #
-            n_things = len(things)
-            print(f'\tNumber of things: {n_things}')
+            pulse_count = press_button(circuit, 1000)
+            print(f'\t1. The number of high * low pulses is: {pulse_count}')
 
             #
             # Part Two
