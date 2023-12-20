16407465525ef5457518c84a3c577e72a71608ce
Stephen Houser <stephenhouser@gmail.com>
2023-12-19 21:54:14 -0500

    Day 19, cleanup

--- Day-19/aplenty.py
+++ Day-19/aplenty.py
@@ -8,7 +8,7 @@ import re
 import argparse
 import unittest
 from cProfile import Profile
-from functools import reduce
+from functools import reduce, partial
 
 class TestAOC(unittest.TestCase):
     """Test Advent of Code"""
@@ -18,22 +18,26 @@ class TestAOC(unittest.TestCase):
     #
     def test_part1_example(self):
         """Test example 1 data from test.txt"""
-        self.assertEqual(test_function('test.txt', 2), 0)
+        workflows, parts = load_file('test.txt')
+        self.assertEqual(rating_sum(workflows, parts), 19114)
 
     def test_part1_solution(self):
         """Live data for part 1 data from input.txt"""
-        self.assertEqual(test_function('input.txt', 200), 0)
+        workflows, parts = load_file('input.txt')
+        self.assertEqual(rating_sum(workflows, parts), 331208)
 
     #
     # Part Two
     #
     def test_part2_example(self):
         """Test example 1 data from test.txt"""
-        self.assertEqual(test_function('test.txt', 10), 0)
+        workflows, _ = load_file('test.txt')
+        self.assertEqual(accepted_combinations(workflows), 167_409_079_868_000)
 
     def test_part2_solution(self):
         """Test example 1 data from test.txt"""
-        self.assertEqual(test_function('input.txt', 1000000), 0)
+        workflows, _ = load_file('input.txt')
+        self.assertEqual(accepted_combinations(workflows), 121_464_316_215_623)
 
 
 class Rule:
@@ -71,6 +75,73 @@ class Rule:
 
         return self.destination
 
+workflow_re = re.compile(r'([a-z]+){(.*)}')
+condition_re = re.compile(r'([asmx])([><])(\d+):([a-zAR]+)')
+def parse_workflow(text):
+    """Return a parsed "workflow" which is a list of Rules"""
+    match = workflow_re.match(text)
+    workflow_name = match.group(1)
+
+    rules = []
+    conditions = match.group(2).split(',')
+    for condition in conditions:
+        cond_match = condition_re.match(condition)
+        if cond_match:
+            variable = cond_match.group(1)
+            comparison = cond_match.group(2)
+            value = int(cond_match.group(3))
+            destination = cond_match.group(4)
+            rules.append(Rule(destination, variable, comparison, value))
+        else:
+            rules.append(Rule(condition, None, None, None))
+
+    return (workflow_name, rules)
+
+part_re = re.compile(r'([asmx])=(\d+)')
+def parse_part(text):
+    """Return a "part" parsed from text"""
+    return {x : int(y) for x, y in part_re.findall(text)}
+
+def load_file(filename: str):
+    """Load lines from file into workflows(rules) and parts"""
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            (workflow_text, instructions_text) = file.read().split('\n\n')
+            # don't have to use comprehension here, R1721
+            #workflows = {x: y for x, y in map(parse_workflow, workflow_text.split('\n'))}
+            workflows = dict(map(parse_workflow, workflow_text.split('\n')))
+            parts = map(parse_part, instructions_text.split('\n'))
+            return (workflows, list(parts))
+
+    except FileNotFoundError:
+        print('File %s not found.', filename)
+
+    return []
+
+# Part One
+def process_part(workflows, part):
+    """Return the "rating number" of the part for accepted parts.
+        Returns 0 for rejected parts.
+    """
+    destination = 'in'
+    while destination not in ('A', 'R'):
+        workflow = workflows[destination]
+        for rule in workflow:
+            if result := rule.execute(part):
+                destination = result
+                break
+
+    if destination == 'A':
+        # rating number is sum of part values
+        return sum(part.values())
+
+    return 0
+
+def rating_sum(workflows, parts):
+    """Return the rating sum for all parts in through the workflows"""
+    return sum(map(partial(process_part, workflows), parts))
+
+# Part Two
 def get_accepted(workflows, start_workflow):
     """Returns list of accepted ranges of values for x, m, a, and s"""
 
@@ -108,116 +179,14 @@ def get_accepted(workflows, start_workflow):
 
     return accepted
 
-def accepted_combinations(workflows, start_workflow):
+def accepted_combinations(workflows):
     """Return the sum of the products of the accepted range combinations"""
     def product(a, c):
         """Product for reduce"""
         return a * (c[1]-c[0]+1)
 
-    accepted = get_accepted(workflows, start_workflow)
+    accepted = get_accepted(workflows, 'in')
     return sum(reduce(product, xmas, 1) for xmas in accepted)
-
-def process_part(workflows, start_workflow, part):
-    """Return the sum of the part values for Accepted parts"""
-
-    destination = start_workflow
-    while destination not in ('A', 'R'):
-        workflow = workflows[destination]
-        for rule in workflow:
-            if result := rule.execute(part):
-                destination = result
-                break
-
-    if destination == 'A':
-        return sum(part.values())
-
-    return 0
-
-
-workflow_re = re.compile(r'([a-z]+){(.*)}')
-condition_re = re.compile(r'([asmx])([><])(\d+):([a-zAR]+)')
-def parse_workflow(text):
-    """Return a parsed "workflow" which is a list of Rules"""
-    match = workflow_re.match(text)
-    workflow_name = match.group(1)
-
-    rules = []
-    conditions = match.group(2).split(',')
-    for condition in conditions:
-        cond_match = condition_re.match(condition)
-        if cond_match:
-            variable = cond_match.group(1)
-            comparison = cond_match.group(2)
-            value = int(cond_match.group(3))
-            destination = cond_match.group(4)
-            rules.append(Rule(destination, variable, comparison, value))
-        else:
-            rules.append(Rule(condition, None, None, None))
-
-    return (workflow_name, rules)
-
-
-part_re = re.compile(r'([asmx])=(\d+)')
-def parse_part(text):
-    """Return a "part" parsed from text"""
-    return {x : int(y) for x, y in part_re.findall(text)}
-
-def load_file(filename: str):
-    """Load lines from file into workflows(rules) and parts"""
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            (workflow_text, instructions_text) = file.read().split('\n\n')
-            workflows = {x: y for x, y in map(parse_workflow, workflow_text.split('\n'))}
-            parts = map(parse_part, instructions_text.split('\n'))
-            return (workflows, list(parts))
-
-    except FileNotFoundError:
-        print('File %s not found.', filename)
-
-    return []
-
-
-class Node:
-    def __init__(self, rule):
-        self.rule = rule
-        self.left = None
-        self.right = None
-
-        self.op = None
-        self.value = None
-        self.variable = None
-
-    def __repr__(self):
-        return str(self)
-
-    def mstr(self, level=0):
-        if self.rule in ('A', 'R'):
-            return self.rule + '\n'
-
-        ret =  f'{self.variable} {self.op} {self.value} -> ' + self.left.mstr(level+1)
-        ret += '  '*level + f'{self.variable} !{self.op} {self.value} -> ' + self.right.mstr(level+1)
-        return ret
-
-def make_tree(workflows, rules):
-    # print(f'make_tree: {rules}')
-
-    if len(rules) == 1:
-        if rules[0] in ('A', 'R'):
-            return Node(rules[0])
-        else:
-            return make_tree(workflows, workflows[rules[0]])
-
-    match = condition_re.match(rules[0])
-    node = Node(rules[0])
-
-    node.variable = match.group(1)
-    node.value = int(match.group(3))
-    node.op = match.group(2)
-    destination = match.group(4)
-
-    node.left = make_tree(workflows, [destination])
-    node.right = make_tree(workflows, rules[1:])
-    return node
 
 
 def main():
@@ -232,26 +201,20 @@ def main():
 
         with Profile() as profile:
             (workflows, parts) = load_file(filename)
+            #print('workflows:', workflows)
+            #print('Parts:', parts)
 
             #
             # Part One
             #
-            #print('workflows:', workflows)
-            #print('Parts:', parts)
-            
-            p = []
-            for part in parts:
-                p.append(process_part(workflows, 'in', part))
-
-            print(p)
-            print(sum(p))
+            overall_rating = rating_sum(workflows, parts)
+            print(f'\t1. Sum of all accepted ratings: {overall_rating}')
 
             #
             # Part Two
             #
-            print(accepted_combinations(workflows, 'in'))
-            # t = make_tree(workflows, ['in'])
-            # print(t.mstr())
+            accepted_parts = accepted_combinations(workflows)
+            print(f'\t2. Accepted combinations: {accepted_parts}')
 
         print()
 
